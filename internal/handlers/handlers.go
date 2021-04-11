6dd6ef389b8fb303a3ad8f44cff9b79e96d9fe0a
aldolushkja <aldo.lushkja@gmail.com>
2021-04-11 22:48:06 +0200

    server side form validation pt3

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -115,11 +115,14 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 
 // Reservation render the make reservation form page
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	//remoteIP := r.RemoteAddr
-	//m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
 
 	render.RenderTemplate(w, r, "make-reservations.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
@@ -140,7 +143,8 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
+	form.Required("first_name", "last_name", "email", "phone")
+	form.MinLength("first_name", 3, r)
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
